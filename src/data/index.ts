b65ba045685e32ee7a9b39f4b6c58672416b46c2
bayurexxa17 <bayurexxa17@gmail.com>
2026-07-22 00:39:57 +0700

    Add files via upload

---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+export * from './activities';
+export * from './schedule';
+export * from './committee';
+export * from './statistics';
+export { gallery, galleryCategories } from './gallery';
+export { getSponsorsByTier } from './sponsors';
+export * from './budget';
+export * from './funding';
+export * from './eventTypes';
