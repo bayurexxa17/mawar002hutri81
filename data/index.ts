373568753eb8697d2cfd0f44305b559ddddec626
bayurexxa17 <bayurexxa17@gmail.com>
2026-07-24 11:26:40 +0700

    Delete data directory

--- data/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './activities';
-export * from './schedule';
-export * from './committee';
-export * from './statistics';
-export { gallery, galleryCategories } from './gallery';
-export { getSponsorsByTier } from './sponsors';
-export * from './budget';
-export * from './funding';
-export * from './eventTypes';
