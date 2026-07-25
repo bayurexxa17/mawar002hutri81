d48c0bb1bec7dbe9aa04630fdd10b0ef0c3c5ee3
bayurexxa17 <bayurexxa17@gmail.com>
2026-07-25 12:33:45 +0700

    Update videos.ts

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -13,10 +13,10 @@ export const videos: VideoItem[] = [
     id: 1,
     title: 'Upacara Bendera HUT RI 2024',
     category: 'upacara',
-    thumbnail: 'images/'Upacara Bendera 1.png,
+    thumbnail: 'images/'Upacara Bendera 1.png',
     videoUrl: 'https://www.youtube.com/watch?v=UuPaS81n0xg&list=RDUuPaS81n0xg&start_radio=1',
     duration: '1:46',
-    year: 2024
+    year: 2026
   },
   {
     id: 2,
@@ -25,7 +25,7 @@ export const videos: VideoItem[] = [
     thumbnail: 'https://images.unsplash.com/photo-1530103862676-de8c9debad1d?w=800',
     videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     duration: '3:15',
-    year: 2024
+    year: 2026
   },
   {
     id: 3,
@@ -34,7 +34,7 @@ export const videos: VideoItem[] = [
     thumbnail: 'https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?w=800',
     videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     duration: '8:40',
-    year: 2024
+    year: 2026
   },
   {
     id: 4,
@@ -43,7 +43,7 @@ export const videos: VideoItem[] = [
     thumbnail: 'https://images.unsplash.com/photo-1540575467063-178a50c2df87?w=800',
     videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     duration: '2:30',
-    year: 2024
+    year: 2026
   },
 ];
 
