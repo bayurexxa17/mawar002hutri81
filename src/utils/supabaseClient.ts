6861501d40846d57441944048d6703379656c3c5
bayurexxa17 <bayurexxa17@gmail.com>
2026-07-25 17:14:41 +0700

    Update supabaseClient.ts

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -4,3 +4,4 @@ const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL || '';
 const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
