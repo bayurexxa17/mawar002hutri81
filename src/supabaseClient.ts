f8ce4c4ed31ecab83ecd6715726f45be3c3c501f
bayurexxa17 <bayurexxa17@gmail.com>
2026-07-28 17:39:40 +0700

    Delete src directory

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://afyyozywnupfdcshxjhk.supabase.co'
-const supabaseAnonKey = 'sb_publishable_EMfJHsDHNjTIatH07CuIzA_BopfHXTe'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
